57fac797bc60cde326fd82902f06d40e3f33c635
Dane Madsen <dane_madsen@tuta.io>
2023-07-20 14:39:49 +1000

    fixed all the bugs

--- op_blender_rhubarb.py
+++ op_blender_rhubarb.py
@@ -6,10 +6,33 @@ import io
 import sys
 import select
 import subprocess
-from threading  import Thread
+from threading import Thread
 from queue import Queue, Empty
 import json
 import os
+
+phoneme_translation_dict = {
+    'X': 'phoneme_REST',
+    'A': 'phoneme_AI',
+    'B': 'phoneme_E',
+    'C': 'phoneme_ETC',
+    'D': 'phoneme_O',
+    'E': 'phoneme_U',
+    'F': 'phoneme_FV',
+    'G': 'phoneme_WQ',
+    'H': 'phoneme_L'
+}
+
+def translate_phoneme(context, phoneme):
+    if phoneme in phoneme_translation_dict:
+        property_name = phoneme_translation_dict[phoneme]
+        if hasattr(context.scene.my_tool, property_name):
+            shape_key_index = int(getattr(context.scene.my_tool, property_name))
+            print(shape_key_index)
+            shape_key_name = context.object.data.shape_keys.key_blocks[shape_key_index].name
+            print(shape_key_name)
+            return shape_key_name
+    return None
 
 class RhubarbLipsyncOperator(bpy.types.Operator):
     """Run Rhubarb lipsync"""
@@ -32,31 +55,115 @@ class RhubarbLipsyncOperator(bpy.types.Operator):
         return context.preferences.addons[__package__].preferences.executable_path and \
             context.object.data.shape_keys
 
+    def modal(self, context, event):
+        wm = context.window_manager
+        wm.progress_update(50)
+
+        try:
+            (stdout, stderr) = self.rhubarb.communicate(timeout=1)
+
+            try:
+                result = json.loads(stderr)
+                if result['type'] == 'progress':
+                    print(result['log']['message'])
+                    self.message = result['log']['message']
+
+                if result['type'] == 'failure':
+                    self.report(type={'ERROR'}, message=result['reason'])
+                    return {'CANCELLED'}
+
+            except ValueError:
+                pass
+            except TypeError:
+                pass
+            except json.decoder.JSONDecodeError:
+                print("JSON Decode Error!!!")
+                print("stdout: ", stdout)
+                print("stderr: ", stderr)
+                pass
+
+            self.rhubarb.poll()
+
+            if self.rhubarb.returncode is not None:
+                wm.event_timer_remove(self._timer)
+                results = json.loads(stdout)
+                fps = context.scene.render.fps
+                last_frame = 0
+                last_shape_key_name = None
+
+                for cue in results['mouthCues']:
+                    frame_num = round(cue['start'] * fps)
+                    shape_key_name = translate_phoneme(context, cue['value'])
+                    print("start: {0} frame: {1} value: {2} shape key: {3}".format(cue['start'], frame_num , cue['value'], shape_key_name))
+                
+                    if shape_key_name:
+                        # Set all shape keys to 0.0 at the start of this phoneme.
+                        for key in context.object.data.shape_keys.key_blocks:
+                            key.value = 0.0
+                            key.keyframe_insert('value', frame=frame_num - 1)
+                
+                        # Set the current shape key to 1.0.
+                        context.object.data.shape_keys.key_blocks[shape_key_name].value = 1.0
+                        context.object.data.shape_keys.key_blocks[shape_key_name].keyframe_insert('value', frame=frame_num)
+                
+                        last_shape_key_name = shape_key_name
+                
+                # Set all shape keys to 0.0 after the last phoneme.
+                for key in context.object.data.shape_keys.key_blocks:
+                    key.value = 0.0
+                    key.keyframe_insert('value', frame=frame_num + 1)
+
+
+                if last_shape_key_name:
+                    context.object.data.shape_keys.key_blocks[last_shape_key_name].value = 0.0
+                    context.object.data.shape_keys.key_blocks[last_shape_key_name].keyframe_insert('value', frame=frame_num + 1)
+
+                wm.progress_end()
+                return {'FINISHED'}
+
+            return {'PASS_THROUGH'}
+        except subprocess.TimeoutExpired as ex:
+            return {'PASS_THROUGH'}
+        except json.decoder.JSONDecodeError:
+            print("JSON Decode Error!!!")
+            print("stdout: ", stdout)
+            print("stderr: ", stderr)
+            wm.progress_end()
+            return {'CANCELLED'}
+        except Exception as ex:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            print(template.format(type(ex).__name__, ex.args))
+            wm.progress_end()
+            return {'CANCELLED'}
+
     def invoke(self, context, event):
         preferences = context.preferences
         addon_prefs = preferences.addons[__package__].preferences
-
-        inputfile = bpy.path.abspath(self.sound_file)
-        dialogfile = bpy.path.abspath(self.dialog_file)
+    
+        inputfile = bpy.path.abspath(context.scene.my_tool.sound_file)
+        print(f"Self.sound_file: {context.scene.my_tool.sound_file}")
+        print(f"Inputfile: {inputfile}")
+        if not os.path.isfile(inputfile):
+            print(f"File does not exist: {inputfile}")
+        dialogfile = bpy.path.abspath(context.scene.my_tool.dialog_file)
         recognizer = bpy.path.abspath(addon_prefs.recognizer)
         executable = bpy.path.abspath(addon_prefs.executable_path)
-
+    
         os.chmod(executable, 0o744)
-
+    
         command = [executable, "-f", "json", "--machineReadable", "--extendedShapes", "GHX", "-r", recognizer, inputfile]
-
+    
         if dialogfile:
             command.append("--dialogFile")
             command.append(dialogfile)
-
-        self.rhubarb = subprocess.Popen(command,
-                                        stdout=subprocess.PIPE, universal_newlines=True)
-
+    
+        self.rhubarb = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    
         wm = context.window_manager
         self._timer = wm.event_timer_add(2, window=context.window)
         wm.modal_handler_add(self)
         wm.progress_begin(0, 100)
-
+    
         return {'RUNNING_MODAL'}
 
     def execute(self, context):
@@ -70,15 +177,11 @@ class RhubarbLipsyncOperator(bpy.types.Operator):
         wm = context.window_manager
         wm.event_timer_remove(self._timer)
 
-
-
 def register():
     bpy.utils.register_class(RhubarbLipsyncOperator)
-
 
 def unregister():
     bpy.utils.unregister_class(RhubarbLipsyncOperator)
 
 if __name__ == "__main__":
     register()
-
--- pnl_blender_rhubarb.py
+++ pnl_blender_rhubarb.py
@@ -1,6 +1,6 @@
 import bpy
 from bpy_extras.io_utils import ImportHelper
-from bpy.props import PointerProperty, StringProperty, IntProperty
+from bpy.props import PointerProperty, StringProperty, IntProperty, EnumProperty
 from bpy.types import PropertyGroup
 from . import op_blender_rhubarb
 
@@ -26,6 +26,71 @@ class MyProperties(PropertyGroup):
         name="Start Frame",
         description="Start frame for the lip-sync",
         default=0,
+    )
+
+    def get_shape_keys(self, context):
+        obj = context.object
+        items = [(str(index), key.name, key.name) for index, key in enumerate(obj.data.shape_keys.key_blocks)]
+        return items
+
+    phoneme_AI: EnumProperty(
+        name="Phoneme AI",
+        description="Shape key for the 'AI' phoneme",
+        items=get_shape_keys
+    )
+
+    phoneme_E: EnumProperty(
+        name="Phoneme E",
+        description="Shape key for the 'E' phoneme",
+        items=get_shape_keys
+    )
+
+    phoneme_ETC: EnumProperty(
+        name="Phoneme etc",
+        description="Shape key for the 'etc' phoneme",
+        items=get_shape_keys
+    )
+
+    phoneme_FV: EnumProperty(
+        name="Phoneme FV",
+        description="Shape key for the 'FV' phoneme",
+        items=get_shape_keys
+    )
+
+    phoneme_L: EnumProperty(
+        name="Phoneme L",
+        description="Shape key for the 'L' phoneme",
+        items=get_shape_keys
+    )
+
+    phoneme_MBP: EnumProperty(
+        name="Phoneme MBP",
+        description="Shape key for the 'MBP' phoneme",
+        items=get_shape_keys
+    )
+
+    phoneme_O: EnumProperty(
+        name="Phoneme O",
+        description="Shape key for the 'O' phoneme",
+        items=get_shape_keys
+    )
+
+    phoneme_REST: EnumProperty(
+        name="Phoneme rest",
+        description="Shape key for the 'rest' phoneme",
+        items=get_shape_keys
+    )
+
+    phoneme_U: EnumProperty(
+        name="Phoneme U",
+        description="Shape key for the 'U' phoneme",
+        items=get_shape_keys
+    )
+
+    phoneme_WQ: EnumProperty(
+        name="Phoneme WQ",
+        description="Shape key for the 'WQ' phoneme",
+        items=get_shape_keys
     )
 
 class RhubarbLipsyncPanel(bpy.types.Panel):
@@ -54,6 +119,38 @@ class RhubarbLipsyncPanel(bpy.types.Panel):
 
         row = layout.row()
         row.prop(my_tool, 'start_frame', text='Start frame')
+
+        layout.label(text="Phoneme to Shape Key Mapping:")
+
+        row = layout.row()
+        row.prop(my_tool, 'phoneme_AI', text='AI')
+
+        row = layout.row()
+        row.prop(my_tool, 'phoneme_E', text='E')
+
+        row = layout.row()
+        row.prop(my_tool, 'phoneme_ETC', text='ETC')
+
+        row = layout.row()
+        row.prop(my_tool, 'phoneme_FV', text='FV')
+
+        row = layout.row()
+        row.prop(my_tool, 'phoneme_L', text='L')
+
+        row = layout.row()
+        row.prop(my_tool, 'phoneme_MBP', text='MBP')
+
+        row = layout.row()
+        row.prop(my_tool, 'phoneme_O', text='O')
+
+        row = layout.row()
+        row.prop(my_tool, 'phoneme_REST', text='REST')
+
+        row = layout.row()
+        row.prop(my_tool, 'phoneme_U', text='U')
+
+        row = layout.row()
+        row.prop(my_tool, 'phoneme_WQ', text='WQ')
 
         row = layout.row()
 
